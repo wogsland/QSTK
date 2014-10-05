0b4764bc21690992753502ad332907f830d05f53
Bradley Wogsland <bwogsland@leadspedia.com>
2014-10-05 18:57:31 -0500

    testing

--- Homework/hw1.py
+++ Homework/hw1.py
@@ -139,11 +139,11 @@ e = dt.datetime(2011,12,31)
 best_sharpe = 0
 best_allocation = 0
 for w in range(1, 10):
+    w = round(float(w)/10,1)
+    print "w = " + str(w)
     for x in range(0, 10):
         for y in range(0, 10):
             for z in range(0, 10):
-                w = round(float(w)/10,1)
-                print "w = " + str(w)
                 x = x/10
                 y = y/10
                 z = z/10
