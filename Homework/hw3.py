0eadefc6494a80fcb3b3296ad2c8362df76e1683
Bradley Wogsland <bwogsland@leadspedia.com>
2014-10-19 21:52:51 -0500

    hw3

--- Homework/hw3.py
+++ Homework/hw3.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     print traded + " " + str(shares)
     #trade = d_data['actual_close'][traded] * shares
     #print trade
-  d_data['actual_close']
+  print d_data['actual_close']
 
   # 4. Calculate the cash timeseries
   # 6. Write to CSV
